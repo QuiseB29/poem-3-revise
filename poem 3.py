5fba2dd1031deb669f52c27b7cbaa255be9160b7
QuiseB29 <bmarquise544@gmail.com>
2024-02-17 20:08:22 -0600

    single and multi line comments

---
+++ poem 3.py
@@ -0,0 +1,7 @@
+# This is from my first peoem
+print("Python, oh Python, so clear and so neat.")
+
+#This line is from my second poem
+favorite_quote = """
+keep focus,soon to be free"""
+print(favorite_quote)
